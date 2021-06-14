59bb49dd980a22391f24ff194d0ac5efb8ba6216
TDH55 <taylorhoward1@wustl.edu>
2021-06-14 13:22:49 -0500

    removed console.log

--- src/PieChart.tsx
+++ src/PieChart.tsx
@@ -67,7 +67,6 @@ class PieChart extends AbstractChart<PieChartProps, PieChartState> {
           }
         }
       }
-      console.log(c.item);
       return (
         <G key={Math.random()}>
           <Path
