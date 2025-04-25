668c20ef524f19b4906c11e49cd860270451ba6c
Alex Kalashnik <oleksandrk@softwareplanetgroup.com>
2025-04-25 12:43:42 +0300

    Update build

--- dist/Utils.d.ts
+++ dist/Utils.d.ts
@@ -1,2 +1,3 @@
 export declare function mapValue(x: number, in_min: number, in_max: number, out_min: number, out_max: number): number;
+export declare function hslToRgba(hslString: string): string;
 //# sourceMappingURL=Utils.d.ts.map
